4cb1fd81e13295d64157910188d33c386630f5b1
= <ethanmacdonald04@gmail.com>
2018-11-28 19:29:09 -0400

    everything is living in harmony right now: there is no byte limit worries, i have a pretty sweet stack setup going on, and everything works perfectly. At the entry point, it has now been changed to load stage2 use CHS instead of LBA. It would be a waste of resources for something living at sector 2 anyway.

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 as BOOT16-2.S -o build/BOOT16.o
 as STAGE2.S   -o build/STAGE2.o
+as TESTKRN.S   -o build/TESTKRN.o
 ld -Tlinker.ld --oformat=binary build/BOOT16.o -o build/BOOT16.bin
 ld -Tlinker.ld --oformat=binary build/STAGE2.o -o build/STAGE2.bin
+ld -Tlinker.ld --oformat=binary build/TESTKRN.o -o build/TESTKRN.bin
 ./outtofs.sh
 #qemu-system-i386 -drive format=raw,file=build/fs.img
 qemu-system-i386 -hda build/fs.img
--- outtofs.sh
+++ outtofs.sh
@@ -2,12 +2,13 @@
 #dd if=/dev/zero of=build/fs.img bs=1024 count=720
 #sudo mkdosfs -F 12 build/fs.img 10240
 dd bs=512 if=build/BOOT16.bin of=build/fs.img seek=0 count=1 conv=notrunc
-dd bs=512 if=build/STAGE2.bin of=build/fs.img seek=3 count=1 conv=notrunc
+dd bs=512 if=build/STAGE2.bin of=build/fs.img seek=1 count=2 conv=notrunc
+dd bs=512 if=build/TESTKRN.bin of=build/fs.img seek=4 count=2 conv=notrunc
 (
 echo n
 echo p
 echo 1
-echo 3
+echo 4
 echo 9
 echo a
 echo w
